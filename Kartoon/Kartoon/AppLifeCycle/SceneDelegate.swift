b1864a79c48adedb7abda50fddbea31b4509be08
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2023-03-28 03:22:07 +0300

    Description: App have been created and have added colors, icons, images.

---
+++ Kartoon/Kartoon/AppLifeCycle/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Kartoon
+//
+//  Created by Oktay Tanrıkulu on 28.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    }
+}
+
